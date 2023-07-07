1d327754b258336d8733bbe0b6c81fad44ad2f7e
AndreaMOr <andreamanuel2207@gmail.com>
2023-07-06 18:39:09 -0600

    Add event to change the completed state of the checkbox

--- src/components/TodosList.js
+++ src/components/TodosList.js
@@ -1,10 +1,14 @@
 import TodoItem from './TodoItem';
 
-const TodosList = ({ todosProps }) => {
+const TodosList = ({ todosProps, handleChange }) => {
   return (
     <ul>
       {todosProps.map((todo) => (
-        <TodoItem key={todo.id} itemProp={todo} />
+        <TodoItem 
+          key={todo.id} 
+          itemProp={todo} 
+          handleChange={handleChange} 
+        />
       ))}
     </ul>
   );
--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,28 +1,44 @@
+import { useState } from "react";
 import InputTodo from "./InputTodo";
 import TodosList from "./TodosList";
 
 const TodosLogic = () => {
-  const todos = [
-      {
-        id: 1,
-        title: 'Setup development environment',
-        completed: true,
-      },
-      {
-        id: 2,
-        title: 'Develop website and add content',
-        completed: false,
-      },
-      {
-        id: 3,
-        title: 'Deploy to live server',
-        completed: false,
-      },
-    ];
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      title: 'Setup development environment',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Develop website and add content',
+      completed: false,
+    },
+    {
+      id: 3,
+      title: 'Deploy to live server',
+      completed: false,
+    },
+  ]);
+
+  const handleChange = (id) => {
+    setTodos((prevState) =>
+      prevState.map((todo) => {
+        if (todo.id === id) {
+          return {
+            ...todo,
+            completed: !todo.completed,
+          };
+        }
+        return todo;
+      })
+    );
+  };
+
   return (
     <div>
       <InputTodo />
-      <TodosList todosProps={todos} />
+      <TodosList todosProps={todos} handleChange={handleChange} />
     </div>
   )
 }
