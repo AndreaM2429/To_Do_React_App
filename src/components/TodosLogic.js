1d1cbfd22313a263f4ad0f7b45e45f6832e90e65
AndreaMOr <andreamanuel2207@gmail.com>
2023-07-06 19:33:15 -0600

    Add logic to add new task with a unique id and prevent empty submitions

--- src/components/TodosLogic.js
+++ src/components/TodosLogic.js
@@ -1,21 +1,22 @@
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 import InputTodo from "./InputTodo";
 import TodosList from "./TodosList";
 
 const TodosLogic = () => {
   const [todos, setTodos] = useState([
     {
-      id: 1,
+      id: uuidv4(),
       title: 'Setup development environment',
       completed: true,
     },
     {
-      id: 2,
+      id: uuidv4(),
       title: 'Develop website and add content',
       completed: false,
     },
     {
-      id: 3,
+      id: uuidv4(),
       title: 'Deploy to live server',
       completed: false,
     },
@@ -43,9 +44,18 @@ const TodosLogic = () => {
     ]);
   };
 
+  const addTodoItem = (title) => {
+    const newTodo = {
+      id: uuidv4(),
+      title: title,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+  };
+
   return (
     <div>
-      <InputTodo />
+      <InputTodo addTodoItem={addTodoItem} />
       <TodosList 
         todosProps={todos}
         handleChange={handleChange}
