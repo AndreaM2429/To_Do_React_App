2e196d8471aa7bc474cf89de8b3c96c9b4d8af75
AndreaMOr <andreamanuel2207@gmail.com>
2023-07-07 19:37:43 -0600

    Create the diferent pages for the app

---
+++ src/routes/About.js
@@ -0,0 +1,23 @@
+import { NavLink, Outlet } from 'react-router-dom';
+
+const About = () => {
+  return (
+    <>
+      <h1>About page.</h1>
+      <div className="about">
+        <ul className="about_list">
+          <li>
+            <NavLink to="about-app">About app</NavLink>
+          </li>
+          <li>
+            <NavLink to="about-developer">About developer</NavLink>
+          </li>
+        </ul>
+        <Outlet />
+      </div>
+    </>
+  );
+};
+
+  export default About;
+  
---
+++ src/routes/Home.js
@@ -0,0 +1,13 @@
+import Header from '../components/Header';
+import TodosLogic from '../components/TodosLogic';
+
+const Home = () => {
+  return (
+      <div className="todos">
+        <Header />
+        <TodosLogic />
+      </div>
+  );
+};
+
+export default Home;
