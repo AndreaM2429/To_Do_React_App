0dd1eb4777d135d664b7b1cca622dec92099d044
AndreaMOr <andreamanuel2207@gmail.com>
2023-07-07 19:39:16 -0600

    Import the different pages in the index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { BrowserRouter } from 'react-router-dom';
 import ReactDOM from "react-dom/client";
 import './styles/app.scss';
 
@@ -9,6 +10,8 @@ const root = ReactDOM.createRoot(domContainer);
 
 root.render(
   <React.StrictMode>
-    <TodoApp />
+    <BrowserRouter>
+      <TodoApp />
+    </BrowserRouter>
   </React.StrictMode>
 );  
